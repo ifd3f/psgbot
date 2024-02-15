a296c561a4142a970c1e4dd60f60f43a79243ce8
Astrid Yu <astrid@astrid.tech>
2024-02-15 12:15:14 -0800

    run hindent

--- src/PSGBot/Main.hs
+++ src/PSGBot/Main.hs
@@ -13,114 +13,72 @@ import System.Random.Stateful
 testgrammar :: Env
 testgrammar =
   Env $
-    Map.fromList
-      [ ( Ident "S1",
-          Rule
-            []
-            ( Fix $
-                Fix
-                  <$> CatE
-                    [ RuleE (Ident "NP") [],
-                      RuleE (Ident "VP") []
-                    ]
-            )
-        ),
-        ( Ident "S2",
-          Rule
-            []
-            ( Fix $
-                Fix
-                  <$> RuleE
-                    (Ident "SuedForBeingLamer")
-                    [ RuleE (Ident "NP") [],
-                      RuleE (Ident "NP") []
-                    ]
-            )
-        ),
-        ( Ident "SuedForBeingLamer",
-          Rule
-            [Ident "s1", Ident "s2"]
-            ( Fix $
-                Fix
-                  <$> CatE
-                    [ RuleE (Ident "s1") [],
-                      LitE "sued",
-                      RuleE (Ident "s2") [],
-                      LitE "for being a lamer version of",
-                      RuleE (Ident "s1") []
-                    ]
-            )
-        ),
-        ( Ident "NP",
-          Rule
-            []
-            ( Fix $
-                CatE
-                  [ Fix $ RuleE (Ident "D") [],
-                    maybeE (Fix $ RuleE (Ident "Adj") []),
-                    Fix $ RuleE (Ident "N") []
-                  ]
-            )
-        ),
-        ( Ident "VP",
-          Rule
-            []
-            ( Fix $
-                Fix
-                  <$> CatE
-                    [ RuleE (Ident "V") [],
-                      RuleE (Ident "NP") []
-                    ]
-            )
-        ),
-        ( Ident "V",
-          Rule
-            []
-            (
-                 uniformUnionE
-                     [ Fix $ LitE "licked",
-                      Fix $ LitE "bit",
-                      Fix $ LitE "kissed",
-                      Fix $ LitE "hugged",
-                      Fix $ LitE "patted"
-                    ]
-            )
-        ),
-        ( Ident "N",
-          Rule
-            []
-            (
-                 uniformUnionE
-                    [ Fix $ LitE "cat",
-                      Fix $ LitE "dog",
-                      Fix $ LitE "fox"
-                    ]
-            )
-        ),
-        ( Ident "D",
-          Rule
-            []
-            (
-                 uniformUnionE
-                     [ Fix $ LitE "the",
-                      Fix $ LitE "a",
-                      Fix $ LitE "that"
-                    ]
-            )
-        ),
-        ( Ident "Adj",
-          Rule
-            []
-            (
-                 uniformUnionE
-                    [ Fix $ LitE "silly",
-                      Fix $ LitE "stupid",
-                      Fix $ LitE "furry",
-                      Fix $ LitE "cute"
-                    ]
-            )
-        )
-      ]
+  Map.fromList
+    [ ( Ident "S1"
+      , Rule
+          []
+          (Fix $ Fix <$> CatE [RuleE (Ident "NP") [], RuleE (Ident "VP") []]))
+    , ( Ident "S2"
+      , Rule
+          []
+          (Fix $
+           Fix <$>
+           RuleE
+             (Ident "SuedForBeingLamer")
+             [RuleE (Ident "NP") [], RuleE (Ident "NP") []]))
+    , ( Ident "SuedForBeingLamer"
+      , Rule
+          [Ident "s1", Ident "s2"]
+          (Fix $
+           Fix <$>
+           CatE
+             [ RuleE (Ident "s1") []
+             , LitE "sued"
+             , RuleE (Ident "s2") []
+             , LitE "for being a lamer version of"
+             , RuleE (Ident "s1") []
+             ]))
+    , ( Ident "NP"
+      , Rule
+          []
+          (Fix $
+           CatE
+             [ Fix $ RuleE (Ident "D") []
+             , maybeE (Fix $ RuleE (Ident "Adj") [])
+             , Fix $ RuleE (Ident "N") []
+             ]))
+    , ( Ident "VP"
+      , Rule
+          []
+          (Fix $ Fix <$> CatE [RuleE (Ident "V") [], RuleE (Ident "NP") []]))
+    , ( Ident "V"
+      , Rule
+          []
+          (uniformUnionE
+             [ Fix $ LitE "licked"
+             , Fix $ LitE "bit"
+             , Fix $ LitE "kissed"
+             , Fix $ LitE "hugged"
+             , Fix $ LitE "patted"
+             ]))
+    , ( Ident "N"
+      , Rule
+          []
+          (uniformUnionE [Fix $ LitE "cat", Fix $ LitE "dog", Fix $ LitE "fox"]))
+    , ( Ident "D"
+      , Rule
+          []
+          (uniformUnionE [Fix $ LitE "the", Fix $ LitE "a", Fix $ LitE "that"]))
+    , ( Ident "Adj"
+      , Rule
+          []
+          (uniformUnionE
+             [ Fix $ LitE "silly"
+             , Fix $ LitE "stupid"
+             , Fix $ LitE "furry"
+             , Fix $ LitE "cute"
+             ]))
+    ]
 
 main :: IO ()
 main = do
